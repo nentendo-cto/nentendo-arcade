d6335bc0b66545a41a8a6541a44e0414d005806e
nentendo-cto <168508816+nentendo-cto@users.noreply.github.com>
2024-05-01 09:18:00 -0400

    revert selector js

--- project/js/browserStart.js
+++ project/js/browserStart.js
@@ -26,40 +26,40 @@ function getParameterByName(name) {
 
 
 window.onload = function() {
-	let romValue;
-	let shaderValue;
+	// let romValue;
+	// let shaderValue;
 
 	// This allows a list box to be on the page to load a given nes file local to the site
-	romValue = "roms/Super Mario Bros (E).nes.zip";
-	console.log( "Loading ROM " + romValue );
-	Gui.App.loadRomFromUrl( romValue );
-	// var sel = $('#loadGameComboBox');
-	// if ( sel ) {
-	// 	sel['change'](function(){
-	// 		var value = $(this)['val']();
-	// 		if ( value.length > 0 ) {
-	// 			console.log( "Loading ROM " + value );
-	// 			Gui.App.loadRomFromUrl( value );
-	// 		}
-	// 	});
-	// }
+	// romValue = "roms/Super Mario Bros (E).nes.zip";
+	console.log( "Loading ROM " + value );
+	Gui.App.loadRomFromUrl( value );
+	var sel = $('#loadGameComboBox');
+	if ( sel ) {
+		sel['change'](function(){
+			var value = $(this)['val']();
+			if ( value.length > 0 ) {
+				console.log( "Loading ROM " + value );
+				Gui.App.loadRomFromUrl( value );
+			}
+		});
+	}
 	
 	// And this is the select box for selecting a WebGL shader
-	shaderValue = "shaders/v1.0/CRT.xml";
-	console.log( "Loading shader " + shaderValue );
-	Gui.App.loadShaderFromUrl( shaderValue );
-	// sel = $('#shaderListComboBox');
-	// if ( sel ) {
-	// 	sel['change'](function(){
-	// 		var value = $(this)['val']();
-	// 		if ( value.length > 0 ) {
-	// 			console.log( "Loading shader " + value );
-	// 			Gui.App.loadShaderFromUrl( value );
-	// 		} else {
-	// 			Gui.App.loadShaderFromUrl( null );
-	// 		}
-	// 	});
-	// }
+	// shaderValue = "shaders/v1.0/CRT.xml";
+	console.log( "Loading shader " + value );
+	Gui.App.loadShaderFromUrl( value );
+	sel = $('#shaderListComboBox');
+	if ( sel ) {
+		sel['change'](function(){
+			var value = $(this)['val']();
+			if ( value.length > 0 ) {
+				console.log( "Loading shader " + value );
+				Gui.App.loadShaderFromUrl( value );
+			} else {
+				Gui.App.loadShaderFromUrl( null );
+			}
+		});
+	}
 	
 	var requestedGameToLoad = getParameterByName( 'gameUrl' );
 	Gui.App.start( { createGuiComponents: true, loadUrl: requestedGameToLoad } );
