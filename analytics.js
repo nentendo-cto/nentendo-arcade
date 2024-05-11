98cf630393880acbb6ed8c6fc5201e6d63e31855
nentendo-cto <168508816+nentendo-cto@users.noreply.github.com>
2024-05-11 00:04:24 -0400

    analytics

---
+++ analytics.js
@@ -0,0 +1,10 @@
+// var mixpanel = require('mixpanel-browser');
+// const mixpanelToken = '0274d77895a7bb80dd6683709df308ea';
+
+// console.log("loading analytics " + mixpanelToken);
+// if (!mixpanel) {
+//     console.log("Mixpanel not found.")
+// } else {
+//     console.log("analytics loaded successfully");
+//     mixpanel.init(mixpanelToken);
+// }
